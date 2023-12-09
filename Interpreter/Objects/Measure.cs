839ddebb9f58dd42b26f4f31cb85b5d7b51b05e4
Pablo Gómez Vidal <123096717+theleywin@users.noreply.github.com>
2023-12-09 11:22:40 -0500

    create measure object

--- Interpreter/Objects/Measure.cs
+++ Interpreter/Objects/Measure.cs
@@ -1,11 +1,26 @@
 ﻿using System;
-namespace Interpreter.Objects
+using System.Numerics;
+namespace Interpreter
 {
 	public class Measure
 	{
-		public Measure()
+		
+		public double Value; // el valor de la medida
+		public Measure(Point p1 , Point p2)
 		{
+			double x1 = p1.X;
+			double y1 = p1.Y;
+			double x2 = p2.X;
+			double y2 = p2.Y;
+			Value = Math.Sqrt(Math.Pow(x2-x1, 2) + Math.Pow(y2-y1,2)); //distancia de punto a punto
 		}
+		
+		public Measure(double value)
+		{
+			Value = value;
+		}
+		
+		
 	}
 }
 
