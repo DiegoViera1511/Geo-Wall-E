619a73f6e2d12952ef94ab2af80cfa227d989c54
Pablo Gómez Vidal <123096717+theleywin@users.noreply.github.com>
2023-12-09 12:33:05 -0500

    create Constrains object

--- Interpreter/Objects/Equation.cs
+++ Interpreter/Objects/Equation.cs
@@ -3,9 +3,41 @@ namespace Interpreter
 {
 	public class Equation
 	{
+		public Point point;
+		
 		public Equation()
 		{
 		}
+	}
+	
+	public class Constrains
+	{
+		public string? ParameterName { get; }
+		public double LowerLimit { get; }
+		public double UpperLimit { get; }
+		 public bool IsInRange(double parameter) => parameter >= LowerLimit && parameter <= UpperLimit;
+		 
+		 public static bool AcceptConstrains(double parameter , Constrains?[] constrains)
+		 {
+		 	foreach(var constrain in constrains)
+			{
+				if(constrain is not null)
+				{
+					if(!constrain.IsInRange(parameter)) return false;
+				}
+			}
+			return true;	
+		 }
+		 
+		  public Constrains(string parameterName, double lowerLimit, double upperLimit)
+	{
+		ParameterName = parameterName;
+		LowerLimit = lowerLimit < upperLimit ? lowerLimit : upperLimit;
+		UpperLimit = upperLimit > lowerLimit ? upperLimit : lowerLimit;
+	}
+	
+	
+	
 	}
 }
 
