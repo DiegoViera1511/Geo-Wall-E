67a02af509ed890ab5d85b4f287a67b725d0df10
Pablo Gómez Vidal <123096717+theleywin@users.noreply.github.com>
2023-12-09 10:34:02 -0500

    create vector object

--- Interpreter/Objects/Vector.cs
+++ Interpreter/Objects/Vector.cs
@@ -3,9 +3,35 @@ namespace Interpreter.Objects
 {
 	public class Vector
 	{
-		public Vector()
+
+		public double X { get; }
+		public double Y { get; }
+		public double Rule { get; } //norma  del vector
+
+
+		public Vector(double x, double y)
 		{
+			X = x;
+			Y = y;
+			Rule = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
 		}
+
+		public Vector VectorRuled() //normalizar un vector
+		{
+			double x = X / Rule;
+			double y = Y / Rule;
+
+			return new Vector(x, y);
+		}
+
+		public Vector MultiplyByScalar (double scalar , Vector vec) //multiplicación por un escalar
+			{
+
+            double x = vec.X * scalar;
+            double y = vec.Y * scalar;
+
+			return new Vector(x, y);
+        }
 	}
 }
 
