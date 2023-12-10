ac5ce59dd1406cee22220101fd3313e6461e2525
Diego Viera <diegovieramartinez@gmail.com>
2023-12-10 10:07:41 -0500

    added Figures GetColor Property

--- Interpreter/Objects/Drawables/Circle.cs
+++ Interpreter/Objects/Drawables/Circle.cs
@@ -6,10 +6,12 @@ public class Circle : Figure
     public Point Center {get => center;}
     private double radio ;
     public double Radio {get => radio;}
-
+    private Color ColorFigure;
+    public override Color GetColor => ColorFigure;
     public Circle(Point center , double radio)
     {
         this.center = center ;
         this.radio = radio ;
+        ColorFigure = Parser.actualColor ;
     }
 }
--- Interpreter/Objects/Drawables/Line.cs
+++ Interpreter/Objects/Drawables/Line.cs
@@ -6,11 +6,13 @@ public class Line : Figure
     public Point P1{get => p1;}
     private Point p2 ;
     public Point P2{get => p2;}
-
+    private Color ColorFigure;
+    public override Color GetColor => ColorFigure;
     public Line(Point p1 , Point p2 )
     {
         this.p1 = p1 ;
         this.p2 = p2 ;
+        ColorFigure = Parser.actualColor ;
     }
 
 }
