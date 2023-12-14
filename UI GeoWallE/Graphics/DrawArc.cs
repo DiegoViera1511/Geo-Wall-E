9ca8ea80ddf76d3d003da6635dfb68061bd560e1
Pablo Gómez Vidal <123096717+theleywin@users.noreply.github.com>
2023-12-14 14:50:40 -0500

    implement the IDrawable interface from maui to my figures

---
+++ UI GeoWallE/Graphics/DrawArc.cs
@@ -0,0 +1,38 @@
+﻿namespace UI_GeoWallE.Graphics
+{
+    public class DrawArc : IDrawable
+    {
+        public Interpreter.Arc arc;
+        public double radius;
+        public Interpreter.Point center;
+        public double startAngle;
+        public double endAngle;
+
+        private static double GetAngle(Interpreter.Point p1, Interpreter.Point p2) //siempre entrar como p1 el centro
+        {
+            double y = (p2.Y - p1.Y);
+            double x = (p2.X - p1.X);
+            double angle = Math.Atan2(y, x);
+            return angle;
+        }
+
+
+        public DrawArc(Interpreter.Arc a)
+        {
+            arc = a;
+            radius = arc.Measure;
+            center = new Interpreter.Point(arc.P1.X - radius, arc.P1.Y - radius);
+            startAngle = (int)(360 - GetAngle(arc.P1, arc.P2) * (180 / Math.PI));
+            endAngle = (int)(360 - GetAngle(arc.P1, arc.P3) * (180 / Math.PI));
+
+        }
+
+        public void Draw(ICanvas canvas, RectF dirtyRect)
+        {
+            canvas.StrokeSize = 4;
+            canvas.StrokeColor = Colors.Black;
+            canvas.DrawArc((float)center.X , (float)center.Y , (float)(2 * radius) ,
+                (float)(2 * radius) , (float)startAngle , (float)endAngle, false , false);
+        }
+    }
+}
---
+++ UI GeoWallE/Graphics/DrawRay.cs
@@ -0,0 +1,64 @@
+﻿namespace UI_GeoWallE.Graphics
+{
+    public class DrawRay : IDrawable
+    {
+        public Interpreter.Ray ray;
+        public double Y1;
+        public double Y2;
+        public double X1;
+        public double X2;
+
+        public DrawRay(Interpreter.Ray r)
+        {
+            ray = r;
+
+            if (ray.P1.X == ray.P2.X)
+            {
+                if (ray.P1.Y < ray.P2.Y)
+                {
+                    X2 = X1 = ray.P1.X;
+                    Y1 = ray.P1.Y;
+                    Y2 = 10000;
+                }
+
+                else
+                {
+                    X2 = X1 = ray.P1.X;
+                    Y1 = ray.P1.Y;
+                    Y2 = -1;
+                }
+            }
+
+            else
+            {
+                double m = (ray.P2.Y - ray.P1.Y) / (ray.P2.X - ray.P1.X);
+                double n = ray.P1.Y - m * ray.P1.X;
+
+                if (ray.P1.X < ray.P2.X)
+                {
+                    X1 = ray.P1.X;
+                    Y1 = ray.P1.Y;
+                    X2 = 10000;
+                    Y2 = 10000 * m + n;
+                }
+
+                else
+                {
+                    X1 = ray.P1.X;
+                    Y1 = ray.P1.Y;
+                    X2 = -1;
+                    Y2 = n;
+                }
+            }
+        }
+
+        public void Draw(ICanvas canvas, RectF dirtyRect)
+        {
+            canvas.StrokeSize = 4;
+            canvas.StrokeColor = Colors.Black;
+            canvas.DrawLine((float)X1, (float)Y1, (float)X2, (float)Y2);
+            canvas.StrokeColor = Colors.Blue;
+            
+        }
+    }
+}
